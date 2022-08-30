53f2fb49993727edd6fa4d66fc5661bfdc71c107
YiZhou <yi.zhou@sansano.usm.cl>
2022-08-30 14:37:42 -0400

    changed vh

--- src/UI/Footer.js
+++ src/UI/Footer.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import styles from './Footer.module.scss'
+import chipleki from '../imgs/chipleki.png'
 const Footer = () => {
+
   return (
     <div className={styles.footer}>
+        <div className={styles.container}>
+            <div className={styles.container__section}>
+                <h2>Desarrollado por: </h2>
+                {/* <img src={chipleki} alt='chipleki'/> */}
+                <p>Encuentranos en</p>
+                <h3> XXX Feria de Software</h3>
+                <p>18 de Noviembre de 2022</p>
+                <p>Universidad Tecnica Federico Santa Maria</p>
+                <p>Av. Espana 1680, Valparaiso</p>
+            </div>
 
+        </div>
     </div>
   )
 }
