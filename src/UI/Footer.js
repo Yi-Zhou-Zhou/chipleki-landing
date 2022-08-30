49f8573cd751b8ca7cfa8519fe8ce8c3b9e4ded4
YiZhou <yi.zhou@sansano.usm.cl>
2022-08-30 18:36:38 -0400

    Added Footer

--- src/UI/Footer.js
+++ src/UI/Footer.js
@@ -1,23 +1,37 @@
 import React from 'react'
 import styles from './Footer.module.scss'
 import chipleki from '../imgs/chipleki.png'
+import usm from '../imgs/usm.png'
+import feria from '../imgs/feria.png'
 const Footer = () => {
 
   return (
-    <div className={styles.footer}>
-        <div className={styles.container}>
-            <div className={styles.container__section}>
-                <h2>Desarrollado por: </h2>
-                {/* <img src={chipleki} alt='chipleki'/> */}
-                <p>Encuentranos en</p>
-                <h3> XXX Feria de Software</h3>
-                <p>18 de Noviembre de 2022</p>
-                <p>Universidad Tecnica Federico Santa Maria</p>
-                <p>Av. Espana 1680, Valparaiso</p>
-            </div>
+    <footer className={styles.footer}>
 
+      <div className={styles.container}>
+        <div className={styles.container__logo}>
+          <img src={chipleki} alt='chipleki team'/>
         </div>
-    </div>
+
+        <div className={styles.container__info}>
+            <h2>Encuentranos en</h2>
+            <p>XXX Feria de Software</p>
+            <p>Universidad Tecnica Federico Santa Maria</p>
+            <p>Av. Espana 1680, Valparaiso</p>
+        </div>
+
+        <div className={styles.container__feed}>
+        <p> Feed</p>
+        </div>
+        <div className={styles.container__logo2}>
+          <img src={usm} alt='chipleki team'/>
+        </div>
+
+        <div className={styles.container__logo3}>
+          <img src={feria} alt='chipleki team'/>
+        </div>
+      </div>
+    </footer>
   )
 }
 
