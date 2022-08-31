63b78f6151fa53eeec37fdd715afabca6e021940
YiZhou <yi.zhou@sansano.usm.cl>
2022-08-31 19:44:08 -0400

    changed footer

--- src/UI/Footer.js
+++ src/UI/Footer.js
@@ -3,33 +3,50 @@ import styles from './Footer.module.scss'
 import chipleki from '../imgs/chipleki.png'
 import usm from '../imgs/usm.png'
 import feria from '../imgs/feria.png'
+import {BsFacebook} from 'react-icons/bs'
+import {AiOutlineInstagram} from 'react-icons/ai'
 const Footer = () => {
-
+  
   return (
     <footer className={styles.footer}>
 
       <div className={styles.container}>
         <div className={styles.container__logo}>
-          <img src={chipleki} alt='chipleki team'/>
+            <div className={styles.container__logo__img}>
+                <img src={chipleki} alt='chipleki team'/>
+            </div>
+
+            <div className={styles.container__logo__info}>
+                <h2>Encuentranos en</h2>
+                <p>XXX Feria de Software</p>
+                <p>Universidad Tecnica Federico Santa Maria</p>
+                <p>Av. Espana 1680, Valparaiso</p>
+            </div>
+          <div className='social__media'>
+            <BsFacebook size={29}/>
+            <AiOutlineInstagram size={29}/>
+            <BsFacebook size={29}/>
+          </div>  
+
         </div>
 
-        <div className={styles.container__info}>
-            <h2>Encuentranos en</h2>
-            <p>XXX Feria de Software</p>
-            <p>Universidad Tecnica Federico Santa Maria</p>
-            <p>Av. Espana 1680, Valparaiso</p>
-        </div>
+        
+            
 
         <div className={styles.container__feed}>
-        <p> Feed</p>
-        </div>
-        <div className={styles.container__logo2}>
-          <img src={usm} alt='chipleki team'/>
+        <div class="fb-page" data-href="https://www.facebook.com/facebook" data-tabs="timeline" data-width="250px" data-height="200px" data-small-header="true" data-adapt-container-width="false" data-hide-cover="true" data-show-facepile="true"><blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div>
         </div>
 
-        <div className={styles.container__logo3}>
-          <img src={feria} alt='chipleki team'/>
+        <div className={styles.container__logos}>
+            <div className={styles.container__logo2}>
+            <img src={usm} alt='utfsm'/>
+            </div>
+
+            {/* <div className={styles.container__logo3}>
+            <img src={feria} alt='feria de software'/>
+            </div> */}
         </div>
+        
       </div>
     </footer>
   )
--- src/UI/Functionalities.js
+++ src/UI/Functionalities.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import styles from './Functionalities.module.scss'
 import search from '../imgs/search.png'
+import testing from '../imgs/testing.png'
+import rank from '../imgs/rank.png'
 const Functionalities = () => {
+    
+
+
   return (
     <div className={styles.functionalities}>
         <div className={styles.container}>
@@ -31,19 +36,21 @@ const Functionalities = () => {
                 </div>
 
                 <div className={styles.container__section__icon}>
-                    <img  src={search} alt='search exercises' className={styles.container__section__icon__image}/>
+                    <img  src={testing} alt='search exercises' className={styles.container__section__icon__image}/>
                 </div> 
             </div>
             <div className={styles.container__section}>
                 <div className={styles.container__section__icon}>
-                    <img  src={search} alt='search exercises' className={styles.container__section__icon__image}/>
+                    <img  src={rank} alt='search exercises' className={styles.container__section__icon__image}/>
                 </div>
                 
                 <div className={styles.container__section__description}>
                     <div className={styles.container__section__description__title}>
                         Lorem Ipsum
                     </div>
-                    <div className={styles.container__section__description__info}>
+                    
+                    
+                     <div className={styles.container__section__description__info}>
                         <h2>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an is simply dummy text of the printing and typesetting industry.</h2>
                     </div>
                 </div>
