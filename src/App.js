babd0e7497d0450fe82c15256cd3b10e7257d758
YiZhou <yi.zhou@sansano.usm.cl>
2022-09-03 04:06:06 -0400

    Changed Font and Responsiveness

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import './App.css';
-import { Router, Routes, Route} from 'react-router-dom';
+import { Router, Routes, Route, BrowserRouter,useLocation} from 'react-router-dom';
 import Home from './UI/Home';
 import Header from './UI/Header';
 import About from './UI/About';
+import AboutHeader from "./UI/AboutHeader"
+import { useLayoutEffect } from 'react'
+
+const Wrapper = ({children}) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+  return children
+} 
+
 function App() {
   return (
+    <BrowserRouter>
+    <Wrapper>
       <Routes>
         <Route path='/' element={<><Header/><Home></Home></>}>
           
         </Route>
 
-        <Route path='/about-us' element={<><Header/><About></About></>}>
+        <Route path='/about-us' element={<><AboutHeader/><About></About></>}>
           
         </Route>
-        
       </Routes>
+      </Wrapper>
+      </BrowserRouter>
   );
 }
 
