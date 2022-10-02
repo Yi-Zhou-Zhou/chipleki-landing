c07ee07b82b7e093bd9bec23701ea502c30d1b2a
Axel Arroyo <axel.arroyo@sansano.usm.cl>
2022-10-02 14:03:10 -0300

    links a redes

--- src/UI/Footer.js
+++ src/UI/Footer.js
@@ -1,50 +1,47 @@
-import React from 'react'
-import styles from './Footer.module.scss'
-import chipleki from '../imgs/chipleki.png'
-import usm from '../imgs/usm.png'
-import feria from '../imgs/feria.png'
-import {FaFacebookSquare} from 'react-icons/fa'
-import {AiFillLinkedin, AiFillInstagram} from 'react-icons/ai'
+import React from "react";
+import styles from "./Footer.module.scss";
+import chipleki from "../imgs/chipleki.png";
+import usm from "../imgs/usm.png";
+import feria from "../imgs/feria.png";
+import { FaFacebookSquare } from "react-icons/fa";
+import { AiFillLinkedin, AiFillInstagram, AiFillMail } from "react-icons/ai";
 
 const Footer = () => {
-  
-  return (
-    <footer className={styles.footer}>
+	return (
+		<footer className={styles.footer}>
+			<div className={styles.container}>
+				<div className={styles.container__logo}>
+					<div className={styles.container__logo__img}>
+						<img src={chipleki} alt="chipleki team" />
+					</div>
 
-      <div className={styles.container}>
-        <div className={styles.container__logo}>
-            <div className={styles.container__logo__img}>
-                <img src={chipleki} alt='chipleki team'/>
-            </div>
+					<div className={styles.container__logo__info}>
+						<h2>Encuéntranos en</h2>
+						<p>XXX Feria de Software</p>
+						<p>Universidad Técnica Federico Santa María</p>
+						<p>Av. España 1680, Valparaíso</p>
+					</div>
+					<div className={styles.social__media}>
+						<a href="https://www.facebook.com/PySano">
+							<FaFacebookSquare size={32} />
+						</a>
+						<a href="https://www.instagram.com/pysano.fdsw/">
+							<AiFillInstagram size={32} />
+						</a>
+						<a href="mailto:contacto.chipleki@gmail.com">
+							<AiFillMail size={32} />
+						</a>
+						{/* <a href='https://linkedin.com'><AiFillLinkedin  size={32}/></a> */}
+					</div>
+				</div>
 
-            <div className={styles.container__logo__info}>
-                <h2>Encuéntranos en</h2>
-                <p>XXX Feria de Software</p>
-                <p>Universidad Técnica Federico Santa María</p>
-                <p>Av. España 1680, Valparaíso</p>
-            </div>
-          <div className={styles.social__media}>
-            <a href='https://facebook.com'><FaFacebookSquare size={32}/></a>
-            <a href='https://instagram.com'><AiFillInstagram size={32}/></a>
-            <a href='https://linkedin.com'><AiFillLinkedin  size={32}/></a>
-            
+				<div className={styles.uLogo}>
+					<img src={usm} alt="usm" />
+					<img src={feria} alt="fesw" />
+				</div>
+			</div>
+		</footer>
+	);
+};
 
-          </div>  
-
-        </div>
-
-      <div className={styles.uLogo}>
-        <img src={usm} alt="usm"/>
-        <img src={feria} alt="fesw"/>
-      </div>
-      
-            
-
-      
-        
-      </div>
-    </footer>
-  )
-}
-
-export default Footer
+export default Footer;
