308cf011e6d6c28e388badd674804f08de624ddc
Axel Arroyo <axel.arroyo@sansano.usm.cl>
2022-10-03 21:05:27 -0300

    styled news

--- src/UI/News.js
+++ src/UI/News.js
@@ -1,14 +1,26 @@
 import React from "react";
-import styles from "./News.module.scss"
+import styles from "./News.module.scss";
 
 const News = () => {
-
-    return (
-        <div className={styles.news} id="news">
-            <iframe title="PySano Facebook" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fpysano&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" className={styles.news__fb} frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
-
-        </div>
-    );
-}
+	return (
+		<div className={styles.news} id="news">
+			<div className={styles.news__group}>
+				<h2 className={styles.news__group__h2}>Noticias</h2>
+				<p className={styles.news__group__p}>
+					Últimas noticias sobre Pysano
+				</p>
+				<hr className={styles.news__group__hr} />
+			</div>
+			<iframe
+				title="PySano Facebook"
+				src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fpysano&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+				className={styles.news__fb}
+				frameborder="0"
+				allowfullscreen="true"
+				allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+			></iframe>
+		</div>
+	);
+};
 
 export default News;
